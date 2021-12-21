567461efb9b874e7de49cefb6b5969ffe41c8dc2
Àlex Cervilla <cervilla.alex@gmail.com>
2021-12-21 18:50:13 +0100

    Fix packages

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="k4pg",
-    version="1.0.0",
+    version="1.0.2",
     author="KeyFr4me",
     description="Pygame utilities used by KeyFr4me",
-    packages=find_packages(include=["k4pg"]),
+    packages=find_packages(),
     install_requires=["pygame>=2.0.1", "colorama"]
 )
