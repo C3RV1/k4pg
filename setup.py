a187ca9cb78fdbe42a6338a7def2d3973e5532a6
KeyFr4me <cervilla.alex@gmail.com>
2022-02-01 14:08:17 +0100

    Version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="k4pg",
-    version="1.0.6",
+    version="1.0.8",
     author="KeyFr4me",
     description="Pygame utilities used by KeyFr4me",
     packages=find_packages(),
